e643ed9b64cfd5e5eabbd79caa1b35cc5e058234
Jack Howard <jackhowaa@gmail.com>
2020-12-11 11:44:24 +0000

    Add variable step count for counter reducer

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -1,13 +1,13 @@
 import * as React from 'react'
 
-function counterReducer(previousCount, newCount) {
-  return newCount;
+function counterReducer(previousCount, step) {
+  return previousCount + step;
 }
 
 function Counter({initialCount = 0, step = 1}) {
   const [count, setCount] = React.useReducer(counterReducer, initialCount)
 
-  const increment = () => setCount(count + step)
+  const increment = () => setCount(step)
   return <button onClick={increment}>{count}</button>
 }
 
